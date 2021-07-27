398e405c699905188077d2cfee6b3251da44f09c
teterkin <teterkin@gmail.com>
2021-07-27 23:26:03 +0500

    Add closures example

---
+++ examples/closures/closures.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	fmt.Println("---")
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+}
---
+++ examples/closures/go.mod
@@ -0,0 +1,3 @@
+module github.com/teterkin/gobyexamples/examples/closures
+
+go 1.16
